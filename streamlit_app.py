68976f2994c607405c2f610f634d573f49b28127
Joseph Benedict Espiritu <75573357+bnespi@users.noreply.github.com>
2023-08-03 16:59:04 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.title('🥣 My Parents New Healthy Diner')
 streamlit.header('🥗 Breakfast Menu')
@@ -16,5 +17,3 @@ streamlit.dataframe(my_fruit_list)
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
-my_fruit_list = my_fruit_list.set_index('Fruit')
-
