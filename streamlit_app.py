25cfa8083beb0bf8c0b69e01c7cac04c1e3e8faa
Joseph Benedict Espiritu <75573357+bnespi@users.noreply.github.com>
2023-08-04 18:19:32 +0800

    Update streamlit_app.py
    added .json()

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,4 +23,4 @@ else:
 
 # Display the table on the page.
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
